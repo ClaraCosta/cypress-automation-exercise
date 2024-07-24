7abae7c4a093f968212c6accf6c6f2486f686169
clara <89088484+ClaraCosta@users.noreply.github.com>
2024-07-24 10:38:03 -0300

    modifications

--- cypress/e2e/accesscart.cy.js
+++ cypress/e2e/accesscart.cy.js
@@ -2,7 +2,7 @@ describe('Access cart', () => {
   it(' 1 - Acessando a home da página Automation Exercise', () => {
     cy.visit('https://www.automationexercise.com/');
     cy.contains(' Cart').children('i').should('have.class', 'fa fa-shopping-cart').click();
-    cy.get('ol').children('li').should('have.class', '');
+    cy.get('ol').children('li').should('have.class', 'active');
     
   })
 })
