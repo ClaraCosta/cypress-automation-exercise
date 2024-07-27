ab0fcc9f617f019f7849ebbfa74d35734a500a3c
clara <89088484+ClaraCosta@users.noreply.github.com>
2024-07-27 16:16:40 -0300

    complete buy

--- cypress/e2e/complete_buy.cy.js
+++ cypress/e2e/complete_buy.cy.js
@@ -4,7 +4,10 @@ describe('Complete buy', () => {
   //==================================================================================================================
   //                                       | COMPLETE BUY TESTS |
   //==================================================================================================================
-    it('1 - Doing login', () => {
+    
+  
+  // LOGIN STEPS
+  it('1 - Doing login and Buying two items', () => {
   
       //Access the URL 
       cy.visit(Cypress.config('baseUrl'));
@@ -21,15 +24,43 @@ describe('Complete buy', () => {
       //Assert the login is completed
       cy.get('ul').children('li', ' Logout');
   
-    })
+
+    // BUY STEPS
   
-    it('2 - Buy two items', () => {
+      //Find the products page
+      cy.get('ul').children('li').contains(' Products').click();
+
+      //Select one item and add to card
+      cy.get('[data-product-id="4"]').contains('Add to cart').click();
+      cy.get("#cartModal").contains('Continue Shopping').click();
+
+      //Select another item and add to card
+      cy.get('[data-product-id="7"]').contains('Add to cart').click();
+      cy.get("#cartModal").contains('View Cart').click();
+      cy.url('https://www.automationexercise.com/view-cart');
+      cy.get('#do_action').contains('Proceed To Checkout').click();
+
   
-      //Action one
-      cy.visit(Cypress.config('baseUrl'));
-  
-      //Action two
-  
+    // REVIEW ORDER
+      cy.contains('Review Your Order');
+      cy.get('textarea').type('Some message for complete the buy');
+      cy.get("#cart_items").contains('Place Order').click();
+
+    //INSERT CARD INFOS
+      cy.url('https://www.automationexercise.com/payment');
+
+      cy.get('[data-qa="name-on-card"]').type('Name od card owner');
+      cy.get('[data-qa="card-number"]').type('1235837837236');
+      cy.get('[data-qa="cvc"]').type('915');
+      cy.get('[data-qa="expiry-month"]').type('12');
+      cy.get('[data-qa="expiry-year"]').type('2070');
+
+      //Finishing the buy
+      cy.get('[data-qa="pay-button"]').click();
+
+      // Assert is done
+      cy.get('[data-qa="order-placed"] > b').should('have.text', 'Order Placed!');
+
   
     })
   
